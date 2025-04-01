a9568128a0960c9bb077f48b8331599a40c62ee1
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-04-01 13:54:44 +0200

    next steps implemented

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,83 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text("Aufgabe 1"),
         ),
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 56, 12, 0),
+            child: Row(
+              children: [
+                Text(
+                  "Hello App Akademie!",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.start,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Container(
+                  child: Center(
+                      child: FilledButton(onPressed: () {}, child: Text("A"))),
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  child: Center(
+                      child: FilledButton(onPressed: () {}, child: Text("B"))),
+                  color: Colors.green,
+                  height: 100,
+                  width: 100,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  child: Center(
+                      child: FilledButton(onPressed: () {}, child: Text("C"))),
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.face,
+                size: 48,
+              ),
+              Icon(
+                Icons.face,
+                size: 48,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          )
+        ]),
       ),
     );
   }
